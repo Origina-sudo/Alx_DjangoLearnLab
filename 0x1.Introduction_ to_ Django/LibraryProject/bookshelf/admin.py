7123cd9725baf9757daf193271c9172533eaf4a2
Origina-sudo <arthurba8585@gmail.com>
2024-09-29 20:47:23 +0000

    u

---
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+
+admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title', 'author')
